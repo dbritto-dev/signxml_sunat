d36e8878d13cb3c611c2085254472105c5075cc6
danilobrinu <ddbn.c2@gmail.com>
2018-12-06 16:53:05 -0500

    Update version on setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name='signxml_sunat',
-    version='1.0.2',
+    version='1.0.3',
     author='@danilobrinu',
     author_email='ddbn.c2@gmail.com',
     description='SUNAT - sign and verify xml',
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst', encoding='utf-8').read(),
     install_requires=[
         'lxml >= 4.2.5',
         'xmlsec >= 1.3.52'
